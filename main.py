de22b3e928d407ed9ce6671b5ebfd400370b1f76
ddurumboy2197 <ddurumboy2197@gmail.com>
2025-12-19 16:31:47 +0500

    Add seasonal greeting based on month input

---
+++ main.py
@@ -0,0 +1,12 @@
+oy = int(input("Oy kirit: "))
+
+if oy == 3 or oy == 4 or oy == 5:
+    print(f'{oy} oy bu Bahor')
+elif oy == 6 or oy == 7 or oy == 8:
+    print(f'{oy} oy bu Yoz')
+elif oy == 9 or oy == 10 or oy == 11:
+    print(f'{oy} oy bu Kuz')
+elif oy == 12 or oy == 1 or oy == 2:
+    print(f'{oy} oy bu Qish')
+else:
+    print(f"{oy} oy fasl yo'q!")
